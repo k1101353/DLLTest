e61d157e913841b94907ab59d716833e459273cb
k1101353 <Jani.Oinonen@seamk.fi>
2015-02-05 18:16:25 +0200

    eka versio

---
+++ DLLTest/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MathLibrary;
+
+namespace DLLTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 6;
+            int b = 2;
+
+            int c = MathMathods.Add(a, b);
+            int d = MathMathods.Multiply(b, a);
+
+            Console.WriteLine(c + " " + d);
+
+        }
+    }
+}
---
+++ MathLibrary/MathMathods.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathLibrary
+{
+    public class MathMathods
+    {
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+        public static int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+    }
+}
